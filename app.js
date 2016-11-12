f6ad99a0c22b341b42279be451d9b70105244ef5
Jem Young <jemuel.young@gmail.com>
2016-11-12 00:39:17 -0600

    Add files

---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require("express");
+const app = express();
+
+// Static css/js files
+app.use('/static', express.static('./dist'))
+
+app.get("/", function(req, res) {
+	res.sendFile( __dirname + '/index.html');
+});
+
+
+const port = 3001;
+
+// Start server
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const path = require('path');
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const less = require('gulp-less');
+const uglify = require('gulp-uglify');
+const minifyCSS = require('gulp-clean-css');
+const del = require('del');
+const shell = require('gulp-shell');
+
+/**
+ * Production css file
+ * @type {String}
+ */
+const CSS_DIST = 'main.min.css';
+/**
+ * Production js file
+ * @type {String}
+ */
+const JS_DIST = 'main.min.js';
+/**
+ * Production folder
+ * @type {String}
+ */
+const DIST_FOLDER = './dist/';
+
+// Concat js files and uglify/minify them
+gulp.task('scripts', function() {
+  gulp.src(['./js/**/*.js'])
+    .pipe(concat(JS_DIST))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest(DIST_FOLDER));
+});
+
+// Convert and concat less > css
+gulp.task('less', function () {
+  gulp.src(['./less/base.less'])
+    .pipe(less())
+    .pipe(concat(CSS_DIST))
+    .pipe(gulp.dest(DIST_FOLDER));
+});
+
+// Remove all js files from the production folder
+gulp.task('clean-js', function() {
+  del([`${DIST_FOLDER}*.js`]).then(paths => {
+    paths.length && console.log('Removed:\n', paths.join('\n'));
+  });
+});
+
+// Remove all css files from the production folder
+gulp.task('clean-css', function() {
+  del([`${DIST_FOLDER}*.css`]).then(paths => {
+    paths.length && console.log('Removed:\n', paths.join('\n'));
+  });
+});
+
+gulp.task('build', ['clean-js', 'clean-css', 'less', 'scripts']);
+
+gulp.task('deploy', ['build']);
